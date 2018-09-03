9ef25b4b3a23d7608dd913ccf7c28ec839e873e4
Emanuel Alarcón <emanuel_cadems@hotmail.com>
2018-09-03 13:09:54 -0300

    Indent code

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -4,8 +4,10 @@ class Rack::Attack
     password: ENV['REDIS_PASSWORD'])
 
 
-  throttle("questions/tenant", limit: ENV['SECOND_LIMIT'].to_i , period: ENV['SECOND_PERIOD'].to_i.seconds) do |req|
-    if req.first_count > req.first_limit && req.path == '/v1/questions' && req.get?
+  throttle("questions/tenant", limit: ENV['SECOND_LIMIT'].to_i ,
+    period: ENV['SECOND_PERIOD'].to_i.seconds) do |req|
+    if req.first_count > req.first_limit &&
+       req.path == '/v1/questions' && req.get?
       req.env['HTTP_AUTHORIZATION']
     end
   end
